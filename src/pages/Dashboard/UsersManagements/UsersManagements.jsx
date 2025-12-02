a832e4646212bc3dd64fa61f73909b4d7529527f
nazrul islam <nazrulislamtube@gmail.com>
2025-12-02 10:18:22 +0600

    m 65-6 search

--- src/pages/Dashboard/UsersManagements/UsersManagements.jsx
+++ src/pages/Dashboard/UsersManagements/UsersManagements.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { FiShieldOff } from 'react-icons/fi';
 import { FaUserShield } from 'react-icons/fa';
@@ -7,18 +7,15 @@ import Swal from 'sweetalert2';
 
 const UsersManagements = () => {
     const axiosSecure = useAxiosSecure();
-    const {refetch ,data : users = []} = useQuery({
-        queryKey: ['users'],
-        queryFn: async ()=>{
-            const res = await axiosSecure.get(`/users`);
+  const [searchText, setSearchText] = useState('')
+     const { refetch, data: users = [] } = useQuery({
+        queryKey: ['users', searchText],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users?searchText=${searchText}`);
             return res.data;
         }
     })
 
-
-    // const handleMakeAdmin = () =>{
-        
-    // }
     const handleMakeAdmin = (user) =>{
         const roleInfo = {role: 'admin'}
         axiosSecure.patch(`/users/${user._id}/role`, roleInfo)
@@ -51,8 +48,9 @@ const UsersManagements = () => {
     return (
         <div>
             <h2 className='text-4xl'>Manage Users: {users.length}</h2>
-            <p>search text: {}</p>
-            <label className="input">
+            <p>search text: {searchText}</p>
+
+ <label className="input">
                 <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <g
                         strokeLinejoin="round"
@@ -66,12 +64,13 @@ const UsersManagements = () => {
                     </g>
                 </svg>
                 <input
-                   
+                    onChange={(e) => setSearchText(e.target.value)}
                     type="search"
                     className="grow"
                     placeholder="Search users" />
 
             </label>
+
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
